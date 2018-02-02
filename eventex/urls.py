91cf2112073574259343e03ac7719e24c121aba7
Hugo <hugo.bernardes@gmail.com>
2018-02-02 19:52:39 -0200

    Thank you page

--- eventex/urls.py
+++ eventex/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from eventex.core.views import home
-from eventex.subscriptions.views import subscribe
+from eventex.subscriptions.views import subscribe, detail
 
 urlpatterns = [
     url(r'^$', home),
                           #view
     url(r'^inscricao/$', subscribe),#raw string -> string "crua" que não suporta codes
+    url(r'^inscricao/(\d+)/$', detail),
     url(r'^admin/', include(admin.site.urls)),
 ]
